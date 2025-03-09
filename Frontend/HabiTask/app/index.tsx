f09b65f73a248bf909ca0c0b1e76fcab847d4a26
l-fraga2811 <lucasbelter2805@gmail.com>
2025-03-09 18:52:47 -0300

    initial commit

---
+++ Frontend/HabiTask/app/index.tsx
@@ -0,0 +1,13 @@
+import { SafeAreaView, Text, View } from "react-native";
+import React from "react";
+
+const Index= () => {
+  return (<SafeAreaView>
+    <View >
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  </SafeAreaView>
+  );
+}
+
+export default Index;
