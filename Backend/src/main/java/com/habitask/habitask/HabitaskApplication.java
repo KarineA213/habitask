882e17db837971862ddda712ebc3f63a7a9c9ab4
l-fraga2811 <lucasbelter2805@gmail.com>
2025-03-13 22:13:48 -0300

    made backend structure

---
+++ Backend/src/main/java/com/habitask/habitask/HabitaskApplication.java
@@ -0,0 +1,13 @@
+package com.habitask.habitask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitaskApplication.class, args);
+	}
+
+}
